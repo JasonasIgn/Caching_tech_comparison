fe5059082ff01dfc77a69a24fa39d938cfc02342
Ignas <ignas.jasonas@telesoftas.com>
2022-05-09 21:27:52 +0300

    Add basic pokemon view components / remove template files

---
+++ rtk-query/src/views/PokemonsView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { PokemonList } from "../components/PokemonList";
+
+export const PokemonListView = () => {
+  return (
+    <div>
+      <h1>Pokemons</h1>
+      <PokemonList />
+    </div>
+  );
+};
